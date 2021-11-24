4082c2fd8c63babfa7d01cf6c8fba8ff8baf772a
robae2821 <89492065+robae2821@users.noreply.github.com>
2021-11-24 14:57:21 -0700

    change index

---
+++ app.js
@@ -0,0 +1,31 @@
+var christmas_date = new Date('Dec 25, 2021 00:00:00')
+
+function countdown(){
+    var today = new Date()
+    var gap = christmas_date - today
+
+    if (gap <= 0) {
+                clearTimeout(setInterval());
+                document.write("Merry Christmas");
+            }
+            
+
+    var seconds = 1000
+    var minutes = seconds * 60
+    var hour = minutes * 60
+    var day = hour *24
+
+    var d = Math.floor(gap / (day))
+    var h = Math.floor((gap % (day)) / (hour))
+    var m = Math.floor((gap % (hour)) / (minutes))
+    var s = Math.floor((gap % (minutes)) / (seconds))
+
+    document.getElementById("days").innerHTML = d
+    document.getElementById("hours").innerHTML = h
+    document.getElementById("minutes").innerHTML = m
+    document.getElementById("seconds").innerHTML = s
+}
+
+setInterval(()=> {
+    countdown()
+}, 1000)
